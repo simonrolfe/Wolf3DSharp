7738e75832cbca82125e418e214677c93d4e49c9
Simon Rolfe <github@simonrolfe.com>
2017-10-14 23:04:25 +0100

    Add basic .NET core bits
    Add the basic bits of a .NET core console project.

---
+++ Wolf3dSharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Wolf3dSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
